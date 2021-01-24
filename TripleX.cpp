4546534b7b0179a99e2de746ff105863fc59ec91
mran754 <68831793+mran754@users.noreply.github.com>
2021-01-24 13:05:12 +1300

    initial commit

---
+++ TripleX.cpp
@@ -0,0 +1,13 @@
+// Simple game in c++ developed in VSCode
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "You are an explorer searching ancient ruins of Kaabal" << endl;
+    cout << "Ten magically sealed doors lead to the treasure room" << endl;
+    cout << "Unlock these doors to collect your prize but beware of traps" << endl;
+    cout << endl;
+    
+    return 0;
+}
