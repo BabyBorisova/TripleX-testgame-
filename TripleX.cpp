aa0221ef865e35dcaa66a80ff3eadfd9f49e1ac6
BabyBorisova <LexiValeriya@gmail.com>
2021-01-25 09:04:18 +0000

    broke up game loop into functions

--- TripleX.cpp
+++ TripleX.cpp
@@ -3,14 +3,14 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	//initializes variables
-	const unsigned int maxDiff = 12;
-	unsigned int diff = 2;
-	unsigned int a, b, c, sum, prod;
-	unsigned int guessA, guessB, guessC, guessSum, guessProd;
+//initializes variables
+const unsigned int maxDiff = 12;
+unsigned int diff = 2;
+unsigned int a, b, c, sum, prod;
+unsigned int guessA, guessB, guessC, guessSum, guessProd;
 
+void PrintIntro()
+{
 	//entry text
 	cout << endl;
   cout << "You are an explorer searching ancient ruins of Ka'bal\n";
@@ -24,38 +24,55 @@ int main()
   cout << "  | \\ / |\n";
   cout << "  |o    |\n";
   cout << "  |_____|\n";
+}
 
-	//game loop
-	while (diff < maxDiff)
+bool PlayGame()
+{
+	//random code generation
+	a = rand()%diff + diff;
+	b = rand()%diff + diff;
+	c = rand()%diff + diff;
+	sum = a + b + c;
+	prod = a * b * c;
+
+	//game info
+	cout << "You approach the door...";
+	cout << "\n* The seal has three runes";
+	cout << "\n* The sum of the runes is " << sum;
+	cout << "\n* The product of the runes is " << prod << endl;
+
+	// takes player guess input
+	cin >> guessA >> guessB >> guessC;
+	guessSum = guessA + guessB + guessC;
+	guessProd = guessA * guessB * guessC;
+	
+	// checks players guess
+	if ((guessSum == sum && guessProd == prod))
 	{
-		//random code generation
-		a = rand()%diff + diff;
-		b = rand()%diff + diff;
-		c = rand()%diff + diff;
-		sum = a + b + c;
-		prod = a * b * c;
-		//game info
-		cout << "You approach the door...";
-		cout << "\n* The seal has three runes";
-		cout << "\n* The sum of the runes is " << sum;
-		cout << "\n* The product of the runes is " << prod;
-
-		// takes player guess input
-		cin >> guessA >> guessB >> guessC;
-		guessSum = guessA + guessB + guessC;
-		guessProd = guessA * guessB * guessC;
-
-		// checks players guess
-		if ((guessSum == sum) && (guessProd == prod))
-		{
-			cout << "\nYou broke the seal" << endl;
-			++diff;
-		}
-		else
-		{
-			cout << "\nThe light of the seal intensifies" << endl;
-			cout << "\nThe seal explodes and knocks you unconcious" << endl;
-		}
+		cout << "\nYou broke the seal\n\n";
+		return true;
 	}
+	else
+	{
+		cout << "\nThe light of the seal intensifies";
+		cout << "\nThe seal explodes and knocks you unconcious\n\n";
+		return false;
+	}
+}
+
+int main()
+{
+	
+
+	PrintIntro();
+
+	while (true)
+	{
+		PlayGame();
+		cin.clear();
+		cin.ignore();
+	}
+
+	
   return 0;
 }
