d9c610442a01d2a7b64ae3fe10270f72247bc79b
BabyBorisova <LexiValeriya@gmail.com>
2021-01-27 04:17:24 +0000

    final game, full game loop with incrementing difficulty

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,12 +1,9 @@
 //Simple game in c++ developed in VSCode
 //BabyBorisova, 2021
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-//initializes variables
-const unsigned int maxDiff = 12;
-unsigned int a, b, c, sum, prod;
-unsigned int guessA, guessB, guessC, guessSum, guessProd;
 
 void PrintIntro()
 {
@@ -25,20 +22,29 @@ void PrintIntro()
   cout << "  |_____|\n";
 }
 
-bool PlayGame(int diff)
+void PrintLevelStats(int difficulty, int sum, int prod)
 {
-	//random code generation
-	a = rand()%diff + diff;
-	b = rand()%diff + diff;
-	c = rand()%diff + diff;
-	sum = a + b + c;
-	prod = a * b * c;
-
-	//game info
-	cout << "You approach the door...";
+	cout << "You approach a door with caster level " << difficulty << " runes...";
 	cout << "\n* The seal has three runes";
 	cout << "\n* The sum of the runes is " << sum;
 	cout << "\n* The product of the runes is " << prod << endl;
+}
+
+bool PlayGame(int difficulty)
+{
+
+	unsigned int a, b, c, sum, prod;
+	unsigned int guessA, guessB, guessC, guessSum, guessProd;
+
+	//random code generation
+	a = rand()%difficulty + difficulty;
+	b = rand()%difficulty + difficulty;
+	c = rand()%difficulty + difficulty;
+	sum = a + b + c;
+	prod = a * b * c;
+
+  //print the level  info
+	PrintLevelStats(difficulty, sum, prod);
 
 	// takes player guess input
 	cin >> guessA >> guessB >> guessC;
@@ -48,47 +54,45 @@ bool PlayGame(int diff)
 	// checks players guess
 	if ((guessSum == sum && guessProd == prod))
 	{
+		cout << "\nYou broke the seal and walk through the door way\n";
 		return true;
 	}
 	else
 	{
+		cout << "\nThe light of the seal intensifies\n";
+		cout << "The seal explodes and knocks you unconcious\n";
 		return false;
 	}
 }
 
 int main()
 {
-	unsigned int diff = 2;
+	srand(time(NULL));
+
+	const unsigned int maxDifficulty = 7;
+	unsigned int levelDifficulty = 1;
 
 	PrintIntro();
 
 	// game loop
-	while (true)
+	while (levelDifficulty <= maxDifficulty)
 	{
-		bool levelComplete = PlayGame(diff);
+		bool levelComplete = PlayGame(levelDifficulty);
 		cin.clear();
 		cin.ignore();
 
 		if (levelComplete) 
 		{
-			++diff;
-			if (diff > maxDiff)
-			{
-				cout << "\nGolden light shines beyond the final door\nYou hvae made it to the tresure room\n";
-				break;
-			}
-			else
-			{
-				cout << "\nYou broke the seal and approach the next door\n";
-			}
+			++levelDifficulty;
 		}
 		else
 		{
-			cout << "\nThe light of the seal intensifies\n";
-			cout << "The seal explodes and knocks you unconcious\n";
-			break;
+			return 0; //exit program
 		}
 	}
+
+	//print win statement
+	cout << "\nGolden light shines beyond the final door\nYou hvae made it to the tresure room\n";
 
 	
   return 0;
